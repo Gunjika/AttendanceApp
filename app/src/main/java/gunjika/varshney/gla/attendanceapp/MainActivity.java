edf6b9e7857b9d3f9017300e32ebe5554bc875b9
Gunjika <gunjikavrshn23@gmail.com>
2019-04-04 21:47:39 +0530

    add new account activity in the project

--- app/src/main/java/gunjika/varshney/gla/attendanceapp/MainActivity.java
+++ app/src/main/java/gunjika/varshney/gla/attendanceapp/MainActivity.java
@@ -2,12 +2,31 @@ package gunjika.varshney.gla.attendanceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textEmail,textPass;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textEmail=findViewById(R.id.editText);
+        textPass=findViewById(R.id.editText2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String c1=textEmail.getText().toString().trim();
+                String c2=textPass.getText().toString().trim();
+                if(c1.equals() && c2.equals())
+                {
+
+                }
+            }
+        });
     }
 }
